c9fddc196763abaf5bc6cae4bc504ce7a671302f
mdariful79 <mdariful057@gmail.com>
2023-04-11 02:14:49 +0600

    Using UseState and react dev tool

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,25 @@
-const home = () => {
+import { useState } from "react";
+
+const Home = () => {
+    // let name = 'Arif';
+    const [name, setName] = useState('Arif');
+    const [age, setAge] = useState('24');
 
     const handleClick = () => {
-        console.log('Hello, Tech World');
+        setName('Akib');
+        setAge(18);
+        
     }
     
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>Click Me</button>
-           
+            
         </div>
      );
 }
  
-export default home;
+export default Home;
