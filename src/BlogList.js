a066c17ecf9353d152c241647853ab38f8084edf
mdariful79 <mdariful057@gmail.com>
2023-04-14 02:28:54 +0600

    JsonServer_using & Fatching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,title, handleDelete}) => {
+const BlogList = ({blogs,title }) => {
 
     return ( 
         <div className="blog-list">
@@ -7,7 +7,7 @@ const BlogList = ({blogs,title, handleDelete}) => {
             <div className="blog-preview" key={blogs.id}>
                 <h2>{blogs.title}</h2>
                 <p>written by {blogs.author}</p>
-                <button onClick={() => handleDelete(blogs.id)}>Delete Blog</button>
+                
             </div>
            ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Arif', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Akib', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Arif', id: 3 }
-      ]);
-      
-      const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs);
-      }
+    const [blogs, setBlogs] = useState(null);
+
+
+
+    useEffect(() => {
+     fetch('http://localhost:8000/blogs')
+     .then(res => {
+      return res.json();
+     })
+    .then(Data => {
+      setBlogs(Data);
+    });
+    },[]);
 
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-           <BlogList blogs={blogs.filter((blog) => blog.author === "Arif")} title="Arif's Blogs!" />
+           {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+          
+          
         </div>
      );
 }
